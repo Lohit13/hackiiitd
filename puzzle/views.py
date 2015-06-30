eb307d31a00bf1501e7e64084b927d5ec5829cea
Lohit13 <lohitaksh14059@iiitd.ac.in>
2015-07-01 00:09:43 +0530

    Second level complete

--- puzzle/views.py
+++ puzzle/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic.base import View
+from django.core.context_processors import csrf
 from django.contrib.auth import logout as auth_logout
 from puzzle.models import *
 from os import remove, rename
@@ -12,6 +13,15 @@ def home(request):
 	return render_to_response('1.html')
 
 def helloworld(request):
-	return render_to_response('2.html')
+	args = {}
+	args.update(csrf(request))
+	return render_to_response('2.html',args)
+
+def last(request):
+	if request.POST:
+		return render_to_response('3.html')
+	else:
+		return HttpResponseRedirect('/helloworld/')
+
 
 
